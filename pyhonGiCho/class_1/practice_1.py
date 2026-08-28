6795d8ff554ab753711557072dba34f9e539a565
minsu <chominsu10@gmail.com>
2026-08-28 15:46:33 +0900

    중간저장

---
+++ pyhonGiCho/class_1/practice_1.py
@@ -0,0 +1,106 @@
+# x = 0
+# print(x)
+# x += 2
+# print(x)
+# x *= 3
+# print(x)
+# x %= 3
+# print(x)
+
+
+# x = 1
+# y = 1
+# z = 2
+
+# result = (x == y) and (y == z)
+# print(result)
+# result = (x == y) or (y == z)
+# print(result)
+# result = not (y == z)
+# print(result)
+
+
+# list1 = list(range(1, 4))
+# print(list1)
+
+# list1 = []
+# list2 = [1, 2, 3]
+# list3 = list()
+# list4 = list((1, 2, 3))
+
+# print(list1)
+# print(list2)
+# print(list3)
+# print(list4)
+
+
+# list1 = [1, 'a', True, 1.1]
+# print(list1)
+# print(len(list1))
+# list2 = [1, 2, 3, 4]
+# print(list2[0])
+# print(list2[-1])
+
+
+# arr = [1, 2, 3, 4, 5]
+# arr.append(6)
+# print(arr)
+# value = arr.pop()
+# print(value)
+# print(arr)
+# value = arr.pop(2)
+# print(value)
+# print(arr)
+# arr.remove(4)
+# print(arr)
+
+
+# arr = [1, 2, 3, 4, 5]
+# arr.extend([6, 7, 8])
+# print(arr)
+
+
+# arr = [1, 2, 3, 4, 5]
+
+# arr2 = arr.copy()
+# print(arr2)
+
+
+# arr = [1, 2, 3, 4, 5]
+# arr2 = arr[1:4]
+# print(arr2)
+
+# arr3 = arr[1:5:2]
+# print(arr3)
+
+
+# arr = list(range(1, 11))
+# del arr[2:6]
+# print(arr)
+# arr.clear()
+# print(arr)
+
+
+# arr = [0] * 5
+# print(arr)
+
+# arr2 = [i+1 for i in range(5)]
+# print(arr2)
+
+
+# s = ['d', 'a', 'c', 'f', 'e', 'b']
+# s.sort()
+# print(s)
+# s.sort(reverse=True)
+# print(s)
+# s2 = ['abcd', 'a', 'ab', 'cd', 'abc']
+# s2.sort(key=len)
+# print(s2)
+
+
+s = [1, 5, 3, 9, 11, 7]
+s1 = sorted(s)
+print(s)
+print(s1)
+s.sort()
+print(s)
