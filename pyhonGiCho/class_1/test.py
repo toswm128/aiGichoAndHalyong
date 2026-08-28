8c7bd3d7ff13487c3b4f3063720c181612724547
조민수 <63090043+toswm128@users.noreply.github.com>
2026-08-28 14:11:54 +0900

    1교시

---
+++ pyhonGiCho/class_1/test.py
@@ -0,0 +1,14 @@
+value = 10
+print(value)
+print(10)
+
+name = 'string'
+print("Hello", name)
+
+name = input()
+print("hi", name)
+
+year_str = input()
+year = int(year_str)
+print(type(year_str))
+print(type(year))
