a90d95f2bab225ca28dd1424509169475e05df90
minsu <chominsu10@gmail.com>
2026-09-11 14:31:47 +0900

    do this

--- pyhonGiCho/class_3/p.py
+++ pyhonGiCho/class_3/p.py
@@ -6,14 +6,23 @@
 # hello('def', text="bye")
 
 
-def int_sum(*a):
-    result = 0
-    for i in a:
-        if type(i) is int:
-            result += i
+# def int_sum(*a):
+#     result = 0
+#     for i in a:
+#         if type(i) is int:
+#             result += i
 
-    return result
+#     return result
 
 
-print(int_sum(1, 2, 3, 4, 5))
-print(int_sum(1, '2', True, "ads", 5))
+# print(int_sum(1, 2, 3, 4, 5))
+# print(int_sum(1, '2', True, "ads", 5))
+
+
+def middle(list):
+    i = len(list) // 2
+    return list[i]
+
+
+print(middle([1, 2, 3, 4, 5]))
+print(middle([1, 2, 3, 4]))
